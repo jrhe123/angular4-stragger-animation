b19cd29c7922109dea90efd5c4de80b09a64b708
jrhe123 <keith_he@outlook.com>
2017-07-22 18:14:09 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
 import { Component } from '@angular/core';
 
+// Animations
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	animations: [
+
+		// matched in html
+		trigger('listAnimation', [
+
+			transition('* => *', [
+
+				// optional: items is not static
+				query(':enter', style({opacity: 0}), {optional: true}),
+
+				query(':enter', stagger('300ms',[
+
+					animate('1s ease-in', keyframes([
+						style({opacity: 0, transform: 'translateY(-75px)', offset:0}),
+						style({opacity: 0.5, transform: 'translateY(35px)', offset:0.3}),
+						style({opacity: 1, transform: 'translateY(0px)', offset:1})
+					]))
+				]), {optional: true}),
+
+				query(':leave', stagger('300ms',[
+
+					animate('1s ease-in', keyframes([
+						style({opacity: 1, transform: 'translateY(0px)', offset:0}),
+						style({opacity: 0.5, transform: 'translateY(35px)', offset:0.3}),
+						style({opacity: 0, transform: 'translateY(-75px)', offset:1})
+					]))
+				]), {optional: true}),
+
+			])
+
+		]),
+
+		// Matched in html
+		trigger('explainerAnim', [
+			transition('* => *', [
+				
+				// reference class
+				query('.col', style({ opacity: 0, transform: 'translateX(-40px)' })),
+
+				query('.col', stagger('500ms', [
+					animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+				])),
+
+				query('.col', [
+					animate(1000, style('*'))
+				])
+
+			])
+		])
+
+
+	]
 })
 export class AppComponent {
-  title = 'app';
+
+	items = [];
+
+	constructor(){
+
+		this.items = [
+			"123",
+			"234",
+			"345"
+		];
+
+	}
+
+	pushItem(){
+		this.items.push("new item");
+	}
+
+	removeItem(){
+		this.items.pop();
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+// Components
+
+// Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// Services
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
